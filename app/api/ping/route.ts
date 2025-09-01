e25dd32b35ccc4898df59702bddb44d60a07420c
daligucheng <78447575+daligucheng@users.noreply.github.com>
2025-09-01 19:59:15 +0900

    Delete app/api/ping/route.ts

--- app/api/ping/route.ts
+++
@@ -1,9 +0,0 @@
-// app/api/ping/route.ts
-import { NextResponse } from "next/server";
-
-// （任意）キャッシュに乗らないようにしたい時
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  return NextResponse.json({ ok: true });
-}
